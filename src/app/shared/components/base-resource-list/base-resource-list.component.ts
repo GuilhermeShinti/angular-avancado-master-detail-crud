2db79e35840cc568b91f5eb5946e8e3b8607d47b
Guilherme Shinti <guilhermeshinti@gmail.com>
2019-07-02 21:24:34 -0300

    fix deleteResource()

--- src/app/shared/components/base-resource-list/base-resource-list.component.ts
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -16,7 +16,7 @@ export abstract class BaseResourceListComponent<T extends BaseResourceModel> imp
     )
   }
 
-  deleteResources(resource: T){
+  deleteResource(resource: T){
     const mustDelete = confirm('Deseja realmente excluir este item?');
     if (mustDelete) {
       this.resourceService.delete(resource.id).subscribe(
